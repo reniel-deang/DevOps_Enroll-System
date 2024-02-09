75e8e7087138f4bffa532b851f390b7f0da77f2b
Paul Landicho <paullandicho005@gmail.com>
2024-02-09 12:19:02 +0800

    - Landicho, Elisha Paul
    * ADDING PAGE

--- pages/dashboardcontent/dashboard.php
+++ pages/dashboardcontent/dashboard.php
@@ -151,7 +151,7 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="pages/forms/general.html" class="nav-link">
+                <a href="ManageElement.php" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>General Elements</p>
                 </a>
