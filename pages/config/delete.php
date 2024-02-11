95287c9ff635dea2c54b25e810e670eb942a007c
Paul Landicho <paullandicho005@gmail.com>
2024-02-11 20:11:00 +0800

    - Landicho,Elisha Paul
    * fixing files

--- pages/config/delete.php
+++ pages/config/delete.php
@@ -2,16 +2,22 @@
 
 session_start();
 include "dbcon.php";
+$id = $_POST['id'];
 $delete = $_POST['delete'];
-$link = $_POST['image'];
+$image = $_POST['image'];
 
-echo $link;
-$sql = "DELETE FROM $delete WHERE img= '$link'";
+$path = "../../assets/img/cms-image$image";
+echo $path;
+
+if(!unlink($path)){
+  echo "You have an error";
+}
+
+$sql = "DELETE FROM $delete WHERE id=$id";
 
 if ($conn->query($sql) === TRUE) {
     $_SESSION['alert'] = '<div class="alert alert-danger" role="alert">
     THE CARD IS DELETED.</div>';
-    unlink($link);
     header("Location: ../dashboardcontent/ManageElem.php");
 } else {
   echo "Error deleting record: " . $conn->error;
